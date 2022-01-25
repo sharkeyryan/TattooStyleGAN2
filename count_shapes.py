6eeff9305fc41b2b5b465f5401687162639040c4
Ryan Sharkey <sharkeyryan@gmail.com>
2022-01-25 00:09:38 -0800

    Fix count_shapes.py

--- count_shapes.py
+++ count_shapes.py
@@ -6,16 +6,14 @@ try:
     directory = '/app/raw-images/1024px'
 
     image_count = 0
-    image_shares = 0
-
 
     for filename in os.listdir(directory):
         if filename.endswith(".jpg"):
-            print("Checking Shape of JPG ({})...\n".format(filename))
+            print("\nChecking Shape of JPG ({})... ".format(filename))
 
             img = Image.open(directory + '/' + filename)
             array = np.array(img)
-            print("Shape is... {}\n".format(array.shape)) 
+            print("{}\n".format(array.shape)) 
 
             image_count = image_count + 1
 
@@ -27,4 +25,3 @@ except Exception as e:
     print("Exception Caught\n\n{}".format(e))
 
 print("Images found: {}...\n".format(image_count))
-print("Images resized: {}...\n".format(image_resized))
